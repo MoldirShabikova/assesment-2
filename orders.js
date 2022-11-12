0ad8a042c86057ff23117d719c4af026ed4b8c18
moldir <moldirshb@gmail.com>
2022-11-12 17:11:10 -0600

    orders done

--- orders.js
+++ orders.js
@@ -31,6 +31,19 @@
 
 //CODE HERE
 
+class Ticket{
+    constructor(items, orderTime, customerId){
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerId = customerId;
+        this.status = 'queued'
+    }
+    updateStatus(newStatus){
+        this.status = newStatus
+        console.log(`The order for customer ${this.customerId} is now ${this.status}.`)
+    }
+
+}
 
 
 /*
@@ -45,7 +58,8 @@
 */
 
 //CODE HERE
-
+const firstTicket = new Ticket('pizza, bread and soda', '7:03 PM', 575)
+console.log(firstTicket)
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +68,4 @@
 */
 
 //CODE HERE
+firstTicket.updateStatus('cooking')
